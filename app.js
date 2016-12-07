07af08ace0ae55bc709edbe412cc1b579540137e
jgarrison1204 <jgarrison1204@gmail.com>
2016-12-07 09:44:47 -0800

    Basic list of purple line subway stations rendering

---
+++ app.js
@@ -0,0 +1,76 @@
+function SubWayListViewModel() {
+	var self = this;
+
+	self.stations = ko.observableArray([
+			{
+				"subwayName": "Union Station", 
+				"address": "801 N Vignes St, Los Angeles 90012" 
+			},
+			{
+				"subwayName": "Civic Center", 
+				"address": "101 S Hill St, Los Angeles 90013 "
+			},
+			{
+	 			"subwayName": "Pershing Square", 
+	 			"address": "500 S Hill St, Los Angeles 90013"
+	 		}, 
+	 		{	
+	 			"subwayName": "7th St/Metro Center", 
+				"address": "660 S Figueroa St, Los Angeles 90017"
+			},
+			{
+				"subwayName": "Westlake/MacArthur Park",
+				"address": "660 S Alvarado St, Los Angeles 90057"
+			},
+			{
+				"subwayName": "Wilshire/Vermont", 
+				"address": "3191 Wilshire Bl, Los Angeles 90005" 
+			},
+			{			
+				"subwayName": "Wilshire/Normandie",
+				"address": "3510 Wilshire Bl, Los Angeles 90005"
+			},
+			{
+				"subwayName": "Wilshire/Western", 
+				"address": "3775 Wilshire Bl, Los Angeles 90005"
+			}
+		]);
+}
+
+ko.applyBindings(new SubWayListViewModel());
+
+
+	// self.stations = ko.observableArray([
+	// 	{
+	// 		"subwayName": "Union Station", 
+	// 		"address": "801 N Vignes St, Los Angeles 90012" 
+	// 	},
+	// 	{
+	// 		"subwayName": "Civic Center", 
+	// 		"address": "101 S Hill St, Los Angeles 90013 "
+	// 	},
+	// 	{
+ // 			"subwayName": "Pershing Square", 
+ // 			"address": "500 S Hill St, Los Angeles 90013"
+ // 		}, 
+ // 		{	
+ // 			"subwayName": "7th St/Metro Center", 
+	// 		"address": "660 S Figueroa St, Los Angeles 90017"
+	// 	},
+	// 	{
+	// 		"subwayName": "Westlake/MacArthur Park",
+	// 		"address": "660 S Alvarado St, Los Angeles 90057"
+	// 	},
+	// 	{
+	// 		"subwayName": "Wilshire/Vermont", 
+	// 		"address": "3191 Wilshire Bl, Los Angeles 90005" 
+	// 	},
+	// 	{			
+	// 		"subwayName": "Wilshire/Normandie",
+	// 		"address": "3510 Wilshire Bl, Los Angeles 90005"
+	// 	},
+	// 	{
+	// 		"subwayName": "Wilshire/Western", 
+	// 		"address": "3775 Wilshire Bl, Los Angeles 90005"
+	// 	}
+	// ]);
