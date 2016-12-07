4385e231d27cd0a05d5b77b37a3312e48d50b748
jgarrison1204 <jgarrison1204@gmail.com>
2016-12-07 14:19:44 -0800

    Filtering is functional with comments explaining how each line works

--- app.js
+++ app.js
@@ -43,20 +43,31 @@ function stations(data){
 function SubWayListViewModel() {
 	var self = this;
 	self.stations = ko.observableArray([]);
+	//sets varaible filter to a ko.obesrvable
 	self.filter = ko.observable('');
 
 	allStations.forEach(function(station){
 		self.stations.push( new stations(station))
 	});
 
+	//creates a property on the object SubWayListView with a ko.computed function as a value. 
 	self.filteredItems = ko.computed(function(){
+		//sets local variable filter to the value of self.filter which takes userinput from an input.  makes all entered string lowercase
 		var filter = self.filter().toLowerCase();
+		//if filter has no text (strings) in it call self.stations which returns entire array of stations
 		if (!filter){
 			return self.stations()
+		//if filter then do something else.....
 		} else {
-			console.log("working");
-		}
-	}, self)
+			//returning some ko.utils arrfilter which takes an arry as first parameter and and a function that returns a boolean
+			return ko.utils.arrayFilter(self.stations(), function(item){
+				//takes item.name() which returns the value of the name property of all items in the stations() observableArray and makes the string lowercase. Th search function takes the user input from the input and returns a boolean for each item in the stations() observableArray after each keyup. For search function any value greater than -1 is true. -1 is false. 
+				var test = item.name().toLowerCase().search(filter) > -1;
+				console.log(test);
+				return test;
+			})
+		} 
+	})
 }
 
 ko.applyBindings(new SubWayListViewModel());
